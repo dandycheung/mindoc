60b84fc5452c84dcc0fbe08e5d9df7d0916297e9
Dandy Cheung <dandycheung@hotmail.com>
2019-12-14 23:26:48 +0800

    浏览时页面底部增加了修订信息（如果有）；增加了 ExportMarkdown() 函数供控制器调用。

--- models/DocumentModel.go
+++ models/DocumentModel.go
@@ -1,53 +1,51 @@
 package models
 
 import (
+	"bytes"
+	"fmt"
+	_ "log"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
 	"time"
 
-	"fmt"
-	"strconv"
-	_"log"
-	"regexp"
-	"io/ioutil"
-
+	"github.com/PuerkitoBio/goquery"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	"github.com/lifei6671/mindoc/cache"
 	"github.com/lifei6671/mindoc/conf"
-	"os"
-	"path/filepath"
-	"github.com/PuerkitoBio/goquery"
-	"bytes"
-	"strings"
-	"github.com/lifei6671/mindoc/utils/filetil"
 	"github.com/lifei6671/mindoc/utils"
 	"github.com/lifei6671/mindoc/utils/cryptil"
+	"github.com/lifei6671/mindoc/utils/filetil"
 	"github.com/lifei6671/mindoc/utils/requests"
 	"github.com/lifei6671/mindoc/utils/ziptil"
 )
 
-// Document struct.
 type Document struct {
-	DocumentId   int    `orm:"pk;auto;unique;column(document_id)" json:"doc_id"`
-	DocumentName string `orm:"column(document_name);size(500)" json:"doc_name"`
+	DocumentId   int           `orm:"pk;auto;unique;column(document_id)" json:"doc_id"`
+	DocumentName string        `orm:"column(document_name);size(500)" json:"doc_name"`
 	// Identify 文档唯一标识
-	Identify  string `orm:"column(identify);size(100);index;null;default(null)" json:"identify"`
-	BookId    int    `orm:"column(book_id);type(int);index" json:"book_id"`
-	ParentId  int    `orm:"column(parent_id);type(int);index;default(0)" json:"parent_id"`
-	OrderSort int    `orm:"column(order_sort);default(0);type(int);index" json:"order_sort"`
-	// Markdown markdown格式文档.
-	Markdown string `orm:"column(markdown);type(text);null" json:"markdown"`
-	// Release 发布后的Html格式内容.
-	Release string `orm:"column(release);type(text);null" json:"release"`
-	// Content 未发布的 Html 格式内容.
-	Content    string    `orm:"column(content);type(text);null" json:"content"`
-	CreateTime time.Time `orm:"column(create_time);type(datetime);auto_now_add" json:"create_time"`
-	MemberId   int       `orm:"column(member_id);type(int)" json:"member_id"`
-	ModifyTime time.Time `orm:"column(modify_time);type(datetime);auto_now" json:"modify_time"`
-	ModifyAt   int       `orm:"column(modify_at);type(int)" json:"-"`
-	Version    int64     `orm:"column(version);type(bigint);" json:"version"`
-	//是否展开子目录：0 否/1 是
-	IsOpen     int           `orm:"column(is_open);type(int);default(0)" json:"is_open"`
-	AttachList []*Attachment `orm:"-" json:"attach"`
+	Identify     string        `orm:"column(identify);size(100);index;null;default(null)" json:"identify"`
+	BookId       int           `orm:"column(book_id);type(int);index" json:"book_id"`
+	ParentId     int           `orm:"column(parent_id);type(int);index;default(0)" json:"parent_id"`
+	OrderSort    int           `orm:"column(order_sort);default(0);type(int);index" json:"order_sort"`
+	// Markdown markdown 格式文档
+	Markdown     string        `orm:"column(markdown);type(text);null" json:"markdown"`
+	// Release 发布后的 Html 格式内容
+	Release      string        `orm:"column(release);type(text);null" json:"release"`
+	// Content 未发布的 Html 格式内容
+	Content      string        `orm:"column(content);type(text);null" json:"content"`
+	CreateTime   time.Time     `orm:"column(create_time);type(datetime);auto_now_add" json:"create_time"`
+	MemberId     int           `orm:"column(member_id);type(int)" json:"member_id"`
+	ModifyTime   time.Time     `orm:"column(modify_time);type(datetime);auto_now" json:"modify_time"`
+	ModifyAt     int           `orm:"column(modify_at);type(int)" json:"-"`
+	Version      int64         `orm:"column(version);type(bigint);" json:"version"`
+	// 是否展开子目录：0 否/1 是/2 空间节点，单击时展开下一级
+	IsOpen       int           `orm:"column(is_open);type(int);default(0)" json:"is_open"`
+	AttachList   []*Attachment `orm:"-" json:"attach"`
 }
 
 // 多字段唯一键
@@ -57,12 +55,12 @@ func (item *Document) TableUnique() [][]string {
 	}
 }
 
-// TableName 获取对应数据库表名.
+// 获取对应数据库表名
 func (item *Document) TableName() string {
 	return "documents"
 }
 
-// TableEngine 获取数据使用的引擎.
+// 获取数据使用的引擎
 func (item *Document) TableEngine() string {
 	return "INNODB"
 }
@@ -77,7 +75,7 @@ func NewDocument() *Document {
 	}
 }
 
-//根据文档ID查询指定文档.
+// 根据文档 ID 查询指定文档
 func (item *Document) Find(id int) (*Document, error) {
 	if id <= 0 {
 		return item, ErrInvalidParameter
@@ -86,7 +84,6 @@ func (item *Document) Find(id int) (*Document, error) {
 	o := orm.NewOrm()
 
 	err := o.QueryTable(item.TableNameWithPrefix()).Filter("document_id", id).One(item)
-
 	if err == orm.ErrNoRows {
 		return item, ErrDataNotExist
 	}
@@ -94,10 +91,11 @@ func (item *Document) Find(id int) (*Document, error) {
 	return item, nil
 }
 
-//插入和更新文档.
+// 插入和更新文档
 func (item *Document) InsertOrUpdate(cols ...string) error {
 	o := orm.NewOrm()
 	item.DocumentName = utils.StripTags(item.DocumentName)
+
 	var err error
 	if item.DocumentId > 0 {
 		_, err = o.Update(item, cols...)
@@ -116,9 +114,11 @@ func (item *Document) InsertOrUpdate(cols ...string) error {
 			sort, _ := o.QueryTable(item.TableNameWithPrefix()).Filter("book_id", item.BookId).Filter("parent_id", item.ParentId).Count()
 			item.OrderSort = int(sort) + 1
 		}
+
 		_, err = o.Insert(item)
 		NewBook().ResetDocumentNumber(item.BookId)
 	}
+
 	if err != nil {
 		return err
 	}
@@ -126,7 +126,7 @@ func (item *Document) InsertOrUpdate(cols ...string) error {
 	return nil
 }
 
-//根据文档识别编号和项目id获取一篇文档
+// 根据文档识别编号和项目 ID 获取一篇文档
 func (item *Document) FindByIdentityFirst(identify string, bookId int) (*Document, error) {
 	o := orm.NewOrm()
 
@@ -135,15 +135,15 @@ func (item *Document) FindByIdentityFirst(identify string, bookId int) (*Documen
 	return item, err
 }
 
-//递归删除一个文档.
+// 递归删除一个文档
 func (item *Document) RecursiveDocument(docId int) error {
-
 	o := orm.NewOrm()
 
 	if doc, err := item.Find(docId); err == nil {
 		o.Delete(doc)
 		NewDocumentHistory().Clear(doc.DocumentId)
 	}
+
 	var maps []orm.Params
 
 	_, err := o.Raw("SELECT document_id FROM " + item.TableNameWithPrefix() + " WHERE parent_id=" + strconv.Itoa(docId)).Values(&maps)
@@ -163,39 +163,35 @@ func (item *Document) RecursiveDocument(docId int) error {
 	return nil
 }
 
-//将文档写入缓存
+// 将文档写入缓存
 func (item *Document) PutToCache() {
 	go func(m Document) {
-
 		if m.Identify == "" {
-
-			if err := cache.Put("Document.Id."+strconv.Itoa(m.DocumentId), m, time.Second*3600); err != nil {
+			if err := cache.Put("Document.Id." + strconv.Itoa(m.DocumentId), m, time.Second * 3600); err != nil {
 				beego.Info("文档缓存失败:", m.DocumentId)
 			}
 		} else {
-			if err := cache.Put(fmt.Sprintf("Document.BookId.%d.Identify.%s", m.BookId, m.Identify), m, time.Second*3600); err != nil {
+			if err := cache.Put(fmt.Sprintf("Document.BookId.%d.Identify.%s", m.BookId, m.Identify), m, time.Second * 3600); err != nil {
 				beego.Info("文档缓存失败:", m.DocumentId)
 			}
 		}
-
 	}(*item)
 }
 
-//清除缓存
+// 清除缓存
 func (item *Document) RemoveCache() {
 	go func(m Document) {
-		cache.Put("Document.Id."+strconv.Itoa(m.DocumentId), m, time.Second*3600)
+		cache.Put("Document.Id." + strconv.Itoa(m.DocumentId), m, time.Second * 3600)
 
 		if m.Identify != "" {
-			cache.Put(fmt.Sprintf("Document.BookId.%d.Identify.%s", m.BookId, m.Identify), m, time.Second*3600)
+			cache.Put(fmt.Sprintf("Document.BookId.%d.Identify.%s", m.BookId, m.Identify), m, time.Second * 3600)
 		}
 	}(*item)
 }
 
-//从缓存获取
+// 从缓存获取
 func (item *Document) FromCacheById(id int) (*Document, error) {
-
-	if err := cache.Get("Document.Id."+strconv.Itoa(id), &item); err == nil && item.DocumentId > 0 {
+	if err := cache.Get("Document.Id." + strconv.Itoa(id), &item); err == nil && item.DocumentId > 0 {
 		beego.Info("从缓存中获取文档信息成功 ->", item.DocumentId)
 		return item, nil
 	}
@@ -203,17 +199,17 @@ func (item *Document) FromCacheById(id int) (*Document, error) {
 	if item.DocumentId > 0 {
 		item.PutToCache()
 	}
-	item, err := item.Find(id)
 
+	item, err := item.Find(id)
 	if err == nil {
 		item.PutToCache()
 	}
+
 	return item, err
 }
 
-//根据文档标识从缓存中查询文档
+// 根据文档标识从缓存中查询文档
 func (item *Document) FromCacheByIdentify(identify string, bookId int) (*Document, error) {
-
 	key := fmt.Sprintf("Document.BookId.%d.Identify.%s", bookId, identify)
 
 	if err := cache.Get(key, item); err == nil && item.DocumentId > 0 {
@@ -226,37 +222,34 @@ func (item *Document) FromCacheByIdentify(identify string, bookId int) (*Documen
 			item.PutToCache()
 		}
 	}()
+
 	return item.FindByIdentityFirst(identify, bookId)
 }
 
-//根据项目ID查询文档列表.
+// 根据项目 ID 查询文档列表.
 func (item *Document) FindListByBookId(bookId int) (docs []*Document, err error) {
 	o := orm.NewOrm()
-
 	_, err = o.QueryTable(item.TableNameWithPrefix()).Filter("book_id", bookId).OrderBy("order_sort").All(&docs)
-
 	return
 }
 
-//判断文章是否存在
+// 判断文章是否存在
 func (item *Document) IsExist(documentId int) bool {
 	o := orm.NewOrm()
-
 	return o.QueryTable(item.TableNameWithPrefix()).Filter("document_id", documentId).Exist()
 }
 
-//发布单篇文档
+// 发布单篇文档
 func (item *Document) ReleaseContent() error {
-
 	item.Release = strings.TrimSpace(item.Content)
 
 	err := item.Processor().InsertOrUpdate("release")
-
 	if err != nil {
 		beego.Error(fmt.Sprintf("发布失败 -> %+v", item), err)
 		return err
 	}
-	//当文档发布后，需要清除已缓存的转换文档和文档缓存
+
+	// 当文档发布后，需要清除已缓存的转换文档和文档缓存
 	item.RemoveCache()
 
 	if err := os.RemoveAll(filepath.Join(conf.WorkingDirectory, "uploads", "books", strconv.Itoa(item.BookId))); err != nil {
@@ -267,17 +260,16 @@ func (item *Document) ReleaseContent() error {
 	return nil
 }
 
-//处理文档的外链，附件，底部编辑信息等.
+// 处理文档的外链，附件，底部编辑信息等
 func (item *Document) Processor() *Document {
 	if item.Release != "" {
 		item.Release = utils.SafetyProcessor(item.Release)
 
-		//安全过滤，移除危险标签和属性
+		// 安全过滤，移除危险标签和属性
 		if docQuery, err := goquery.NewDocumentFromReader(bytes.NewBufferString(item.Release)); err == nil {
-
-			//处理附件
+			// 处理附件
 			if selector := docQuery.Find("div.attach-list").First(); selector.Size() <= 0 {
-				//处理附件
+				// 处理附件
 				attachList, err := NewAttachment().FindListByDocumentId(item.DocumentId)
 				if err == nil && len(attachList) > 0 {
 					content := bytes.NewBufferString("<div class=\"attach-list\"><strong>附件</strong><ul>")
@@ -285,11 +277,13 @@ func (item *Document) Processor() *Document {
 						if strings.HasPrefix(attach.HttpPath, "/") {
 							attach.HttpPath = strings.TrimSuffix(conf.BaseUrl, "/") + attach.HttpPath
 						}
-						li := fmt.Sprintf("<li><a href=\"%s\" target=\"_blank\" title=\"%s\">%s</a></li>", attach.HttpPath, attach.FileName, attach.FileName)
 
+						li := fmt.Sprintf("<li><a href=\"%s\" target=\"_blank\" title=\"%s\">%s</a></li>", attach.HttpPath, attach.FileName, attach.FileName)
 						content.WriteString(li)
 					}
+
 					content.WriteString("</ul></div>")
+
 					if docQuery == nil {
 						docQuery, err = goquery.NewDocumentFromReader(content);
 					} else {
@@ -304,11 +298,24 @@ func (item *Document) Processor() *Document {
 				}
 			}
 
-			//处理了文档底部信息
+			// 处理了文档底部信息
 			if selector := docQuery.Find("div.wiki-bottom").First(); selector.Size() <= 0 && item.MemberId > 0 {
-				//处理文档结尾信息
+				// 处理文档结尾信息
+				release := "<div class=\"wiki-bottom\">"
+
+				if item.ModifyAt > 0 {
+					docModify, err := NewMember().Find(item.ModifyAt, "real_name", "account")
+					if err == nil {
+						if docModify.RealName != "" {
+							release += "最后编辑: " + docModify.RealName + " &nbsp;"
+						} else {
+							release += "最后编辑: " + docModify.Account + " &nbsp;"
+						}
+					}
+				}
+				release += "文档更新时间: " + item.ModifyTime.Local().Format("2006-01-02 15:04") + " &nbsp;&nbsp;作者："
+
 				docCreator, err := NewMember().Find(item.MemberId, "real_name", "account")
-				release := "<div class=\"wiki-bottom\">文档更新时间: " + item.ModifyTime.Local().Format("2006-01-02 15:04") + " &nbsp;&nbsp;作者："
 				if err == nil && docCreator != nil {
 					if docCreator.RealName != "" {
 						release += docCreator.RealName
@@ -316,6 +323,7 @@ func (item *Document) Processor() *Document {
 						release += docCreator.Account
 					}
 				}
+
 				release += "</div>"
 
 				if selector := docQuery.Find("div.markdown-article").First(); selector.Size() > 0 {
@@ -325,33 +333,70 @@ func (item *Document) Processor() *Document {
 				}
 			}
 
-			//设置图片为CDN地址
-			if cdnimg := beego.AppConfig.String("cdnimg"); cdnimg != "" {
-				docQuery.Find("img").Each(func(i int, contentSelection *goquery.Selection) {
-					if src, ok := contentSelection.Attr("src"); ok && strings.HasPrefix(src, "/uploads/") {
-						contentSelection.SetAttr("src", utils.JoinURI(cdnimg, src))
+			cdnimg := beego.AppConfig.String("cdnimg")
+
+			docQuery.Find("img").Each(func(i int, selection *goquery.Selection) {
+				if src, ok := selection.Attr("src"); ok {
+					src = strings.TrimSpace(strings.ToLower(src))
+
+					// 过滤掉没有链接的图片标签
+					if src == "" || strings.HasPrefix(src, "data:text/html") {
+						selection.Remove()
+						return
 					}
-				})
-			}
+
+					// 设置图片为 CDN 地址
+					if cdnimg != "" && strings.HasPrefix(src, "/uploads/") {
+						selection.SetAttr("src", utils.JoinURI(cdnimg, src))
+					}
+				}
+				selection.RemoveAttr("onerror").RemoveAttr("onload")
+			})
+
+			// 过滤 A 标签的非法连接
+			docQuery.Find("a").Each(func(i int, selection *goquery.Selection) {
+				if val, exists := selection.Attr("href"); exists {
+					if val == "" {
+						selection.SetAttr("href", "#")
+						return
+					}
+
+					val = strings.Replace(strings.ToLower(val), " ", "", -1)
+
+					// 移除危险脚本链接
+					if strings.HasPrefix(val, "data:text/html") ||
+						strings.HasPrefix(val, "vbscript:") ||
+						strings.HasPrefix(val, "&#106;avascript:") ||
+						strings.HasPrefix(val, "javascript:") {
+						selection.SetAttr("href", "#")
+					}
+				}
+
+				// 移除所有 onerror 属性
+				selection.RemoveAttr("onerror").RemoveAttr("onload").RemoveAttr("onclick")
+			})
+
+			docQuery.Find("script").Remove()
+			docQuery.Find("link").Remove()
+			docQuery.Find("vbscript").Remove()
 
 			if html, err := docQuery.Html(); err == nil {
 				item.Release = strings.TrimSuffix(strings.TrimPrefix(strings.TrimSpace(html), "<html><head></head><body>"), "</body></html>")
 			}
 		}
 	}
+
 	return item
 }
 
 func (item *Document) ExportMarkdown(sessionId string) (string, error) {
-
 	documentId := strconv.Itoa(item.DocumentId)
 	bookId := strconv.Itoa(item.BookId)
 
-	outputPath := filepath.Join(conf.WorkingDirectory, "uploads", "documents", bookId , documentId+".zip")
-
+	outputPath := filepath.Join(conf.WorkingDirectory, "uploads", "documents", bookId , documentId + ".zip")
 	os.MkdirAll(filepath.Dir(outputPath), 0644)
 
-	tempOutputPath := filepath.Join(os.TempDir(), sessionId, "markdown_doc_"+documentId)
+	tempOutputPath := filepath.Join(os.TempDir(), sessionId, "markdown_doc_" + documentId)
 
 	var docPath string
 
@@ -359,10 +404,10 @@ func (item *Document) ExportMarkdown(sessionId string) (string, error) {
 		if strings.HasSuffix(item.Identify, ".md") || strings.HasSuffix(item.Identify, ".markdown") {
 			docPath = filepath.Join(tempOutputPath, item.Identify)
 		} else {
-			docPath = filepath.Join(tempOutputPath, item.Identify+".md")
+			docPath = filepath.Join(tempOutputPath, item.Identify + ".md")
 		}
 	} else {
-		docPath = filepath.Join(tempOutputPath, strings.TrimSpace(item.DocumentName)+".md")
+		docPath = filepath.Join(tempOutputPath, strings.TrimSpace(item.DocumentName) + ".md")
 	}
 
 	dirPath := filepath.Dir(docPath)
@@ -371,7 +416,6 @@ func (item *Document) ExportMarkdown(sessionId string) (string, error) {
 	markdown := item.Markdown
 
 	if strings.TrimSpace(item.Markdown) != "" {
-
 		re := regexp.MustCompile(`!\[(.*?)\]\((.*?)\)`)
 
 		markdown = re.ReplaceAllStringFunc(item.Markdown, func(image string) string {
@@ -379,10 +423,11 @@ func (item *Document) ExportMarkdown(sessionId string) (string, error) {
 			if len(images) <= 0 || len(images[0]) < 3 {
 				return image
 			}
+
 			originalImageUrl := string(images[0][2])
 			imageUrl := strings.Replace(string(originalImageUrl), "\\", "/", -1)
 
-			//如果是本地路径，则需要将图片复制到项目目录
+			// 如果是本地路径，则需要将图片复制到项目目录
 			if strings.HasPrefix(imageUrl, "http://") || strings.HasPrefix(imageUrl, "https://") {
 				imageExt := cryptil.Md5Crypt(imageUrl) + filepath.Ext(imageUrl)
 
@@ -399,24 +444,23 @@ func (item *Document) ExportMarkdown(sessionId string) (string, error) {
 				imageUrl = "."+imageUrl
 			}
 
-			imageUrl = strings.Replace(strings.TrimSuffix(image, originalImageUrl+")")+imageUrl+")", "\\", "/", -1)
+			imageUrl = strings.Replace(strings.TrimSuffix(image, originalImageUrl + ")") + imageUrl + ")", "\\", "/", -1)
 
 			return imageUrl
 		})
-
 	} else {
 		markdown = "# " + item.DocumentName + "\n"
 	}
 
 	if err := ioutil.WriteFile(docPath, []byte(markdown), 0644); err != nil {
-		beego.Error("导出Markdown失败->", err)
+		beego.Error("导出 Markdown 失败 ->", err)
 		return outputPath, err
 	}
 
 	if err := ziptil.Compress(outputPath, tempOutputPath); err != nil {
-		beego.Error("导出Markdown失败->", err)
+		beego.Error("导出 Markdown 失败 ->", err)
 		return "", err
 	}
-	return outputPath, nil
 
+	return outputPath, nil
 }
