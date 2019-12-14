07ef0c1e367e5b2b0ad11e69abb38b002dd8936d
Dandy Cheung <dandycheung@hotmail.com>
2019-12-14 23:46:01 +0800

    与上游主线保持了同步，应该是增强了搜索结果。

--- models/DocumentSearchResult.go
+++ models/DocumentSearchResult.go
@@ -27,13 +27,13 @@ func NewDocumentSearchResult() *DocumentSearchResult {
 	return &DocumentSearchResult{}
 }
 
-//分页全局搜索.
+// 分页全局搜索
 func (m *DocumentSearchResult) FindToPager(keyword string, pageIndex, pageSize, memberId int) (searchResult []*DocumentSearchResult, totalCount int, err error) {
 	o := orm.NewOrm()
 
 	offset := (pageIndex - 1) * pageSize
 
-	keyword = "%" + strings.Replace(keyword," ","%",-1) + "%"
+	keyword = "%" + strings.Replace(keyword, " ", "%", -1) + "%"
 
 	if memberId <= 0 {
 		sql1 := `SELECT count(doc.document_id) as total_count FROM md_documents AS doc
@@ -59,16 +59,34 @@ FROM (
          LEFT JOIN md_relationship AS rel ON book.book_id = rel.book_id AND rel.role_id = 0
          LEFT JOIN md_members AS member ON rel.member_id = member.member_id
        WHERE book.privately_owned = 0 AND (doc.document_name LIKE ? OR doc.release LIKE ?)
+     UNION ALL
+SELECT
+  book.book_id AS document_id,
+  book.modify_time,
+  book.create_time,
+  book.book_name AS document_name,
+  book.identify,
+  book.description,
+  book.identify  AS book_identify,
+  book.book_name,
+  rel.member_id,
+  member.account AS author,
+  'book'     AS search_type
+FROM  md_books AS book
+       LEFT JOIN md_relationship AS rel ON book.book_id = rel.book_id AND rel.role_id = 0
+       LEFT JOIN md_members AS member ON rel.member_id = member.member_id
+WHERE book.privately_owned = 0 AND (book.book_name LIKE ? OR book.description LIKE ?)
+
        UNION ALL
        SELECT
-         blog.blog_id,
+         blog.blog_id AS document_id,
          blog.modify_time,
          blog.create_time,
-         blog.blog_title,
+         blog.blog_title as document_name,
          blog.blog_identify,
          blog.blog_release,
          blog.blog_identify,
-         blog.blog_title,
+         blog.blog_title as book_name,
          blog.member_id,
          member.account,
          'blog' AS search_type
@@ -81,9 +99,10 @@ LIMIT ?, ?;`
 
 		err = o.Raw(sql1, keyword, keyword).QueryRow(&totalCount)
 		if err != nil {
-			beego.Error("查询搜索结果失败 -> ",err)
+			beego.Error("查询搜索结果失败 -> ", err)
 			return
 		}
+
 		sql3 := `       SELECT
          count(*)
        FROM md_blogs AS blog
@@ -92,15 +111,27 @@ LIMIT ?, ?;`
 		c := 0
 		err = o.Raw(sql3, keyword, keyword).QueryRow(&c)
 		if err != nil {
-			beego.Error("查询搜索结果失败 -> ",err)
+			beego.Error("查询搜索结果失败 -> ", err)
 			return
 		}
 
 		totalCount += c
 
-		_, err = o.Raw(sql2, keyword, keyword,keyword,keyword, offset, pageSize).QueryRows(&searchResult)
+		// 查询项目的数量
+		sql4 := `SELECT count(*) as total_count FROM md_books as book
+WHERE book.privately_owned = 0 AND (book.book_name LIKE ? OR book.description LIKE ?);`
+
+		err = o.Raw(sql4, keyword, keyword).QueryRow(&c)
 		if err != nil {
-			beego.Error("查询搜索结果失败 -> ",err)
+			beego.Error("查询搜索结果失败 -> ", err)
+			return
+		}
+
+		totalCount += c
+
+		_, err = o.Raw(sql2, keyword, keyword, keyword, keyword, keyword, keyword, offset, pageSize).QueryRows(&searchResult)
+		if err != nil {
+			beego.Error("查询搜索结果失败 -> ", err)
 			return
 		}
 	} else {
@@ -112,7 +143,7 @@ LIMIT ?, ?;`
                    	from md_team_relationship as mtr
 					left join md_team_member as mtm on mtm.team_id=mtr.team_id and mtm.member_id=? order by role_id desc )as t group by t.role_id,t.team_member_id,t.book_id) as team 
 					on team.book_id = book.book_id
-WHERE (book.privately_owned = 0 OR rel1.relationship_id > 0 or team.team_member_id > 0)  AND (doc.document_name LIKE ? OR doc.release LIKE ?) `
+WHERE (book.privately_owned = 0 OR rel1.relationship_id > 0 or team.team_member_id > 0)  AND (doc.document_name LIKE ? OR doc.release LIKE ?);`
 
 		sql2 := `SELECT *
 FROM (
@@ -148,14 +179,43 @@ FROM (
        UNION ALL
 
        SELECT
-         blog.blog_id,
+         book.book_id AS document_id,
+         book.modify_time,
+         book.create_time,
+         book.book_name AS document_name,
+         book.identify,
+         book.description AS description,
+         book.identify  AS book_identify,
+         book.book_name,
+         rel.member_id,
+         member.account AS author,
+         'book'     AS search_type
+       FROM md_books AS book
+         LEFT JOIN md_relationship AS rel ON book.book_id = rel.book_id AND rel.role_id = 0
+         LEFT JOIN md_members AS member ON rel.member_id = member.member_id
+         LEFT JOIN md_relationship AS rel1 ON book.book_id = rel1.book_id AND rel1.member_id = ?
+         LEFT JOIN (SELECT *
+                    FROM (SELECT
+                            book_id,
+                            team_member_id,
+                            role_id
+                          FROM md_team_relationship AS mtr
+                            LEFT JOIN md_team_member AS mtm ON mtm.team_id = mtr.team_id AND mtm.member_id = ?
+                          ORDER BY role_id DESC) AS t
+                    GROUP BY t.role_id, t.team_member_id, t.book_id) AS team
+           ON team.book_id = book.book_id
+       WHERE (book.privately_owned = 0 OR rel1.relationship_id > 0 OR team.team_member_id > 0) AND
+             (book.book_name LIKE ? OR book.description LIKE ?)
+ UNION ALL
+       SELECT
+         blog.blog_id AS document_id, 
          blog.modify_time,
          blog.create_time,
-         blog.blog_title,
-         blog.blog_identify,
-         blog.blog_release,
-         blog.blog_identify,
-         blog.blog_title,
+         blog.blog_title as document_name,
+         blog.blog_identify as identify,
+         blog.blog_release as description,
+         blog.blog_identify  AS book_identify,
+         blog.blog_title as book_name,
          blog.member_id,
          member.account,
          'blog' AS search_type
@@ -171,6 +231,7 @@ LIMIT ?, ?;`
 		if err != nil {
 			return
 		}
+
 		sql3 := `       SELECT
          count(*)
        FROM md_blogs AS blog
@@ -178,29 +239,48 @@ LIMIT ?, ?;`
              (blog.blog_release LIKE ? OR blog.blog_title LIKE ?);`
 
 		c := 0
-		err = o.Raw(sql3,memberId, keyword, keyword).QueryRow(&c)
+		err = o.Raw(sql3, memberId, keyword, keyword).QueryRow(&c)
+		if err != nil {
+			beego.Error("查询搜索结果失败 -> ", err)
+			return
+		}
+
+		totalCount += c
+
+		sql4 := `SELECT count(*) as total_count FROM md_books as book
+  LEFT JOIN md_relationship AS rel ON book.book_id = rel.book_id AND rel.role_id = 0
+  LEFT JOIN md_relationship AS rel1 ON book.book_id = rel1.book_id AND rel1.member_id = ?
+			left join (select * from (select book_id,team_member_id,role_id
+                   	from md_team_relationship as mtr
+					left join md_team_member as mtm on mtm.team_id=mtr.team_id and mtm.member_id=? order by role_id desc )as t group by t.role_id,t.team_member_id,t.book_id) as team
+					on team.book_id = book.book_id
+WHERE (book.privately_owned = 0 OR rel1.relationship_id > 0 or team.team_member_id > 0)  AND (book.book_name LIKE ? OR book.description LIKE ?);`
+
+		err = o.Raw(sql4, memberId, memberId, keyword, keyword).QueryRow(&c)
 		if err != nil {
 			beego.Error("查询搜索结果失败 -> ",err)
 			return
 		}
 
 		totalCount += c
-		_, err = o.Raw(sql2, memberId, memberId, keyword, keyword,memberId,keyword, keyword, offset, pageSize).QueryRows(&searchResult)
+
+		_, err = o.Raw(sql2, memberId, memberId, keyword, keyword, memberId, memberId, keyword, keyword, memberId, keyword, keyword, offset, pageSize).QueryRows(&searchResult)
 		if err != nil {
 			return
 		}
 	}
+
 	return
 }
 
-//项目内搜索.
-func (m *DocumentSearchResult) SearchDocument(keyword string, book_id int) (docs []*DocumentSearchResult, err error) {
+// 项目内搜索
+func (m *DocumentSearchResult) SearchDocument(keyword string, bookId int) (docs []*DocumentSearchResult, err error) {
 	o := orm.NewOrm()
 
 	sql := "SELECT * FROM md_documents WHERE book_id = ? AND (document_name LIKE ? OR `release` LIKE ?) "
 	keyword = "%" + keyword + "%"
 
-	_, err = o.Raw(sql, book_id, keyword, keyword).QueryRows(&docs)
+	_, err = o.Raw(sql, bookId, keyword, keyword).QueryRows(&docs)
 
 	return
 }
